e6e827ce13e4541eb0e256ed5718c10118a3a15f
daltenbern3 <darrington.altenbern@gmail.com>
2017-02-19 15:19:24 -0500

    more meme deletion

--- src/serverHandlers/getOneScenario.handler.js
+++ src/serverHandlers/getOneScenario.handler.js
@@ -1,91 +1,91 @@
 export default function(req, res) {
 	res.send({
 		_id: 1,
-		title: "Test Scenario",
-		description: "This Scenario is only for testing",
-		steps: [
-			{//0
-				type: "dialog",
-				speaker: "narrator",
-				goTo: 1,
-				text: "Welcome to the Test Scenario! Please press next."
-			},
-			{//1
-				type: "dialog",
-				speaker: "doctor",
-				goTo: 2,
-				text: "ayyyyy"
-			},
-			{//2
-				type: "choice",
-				choices: [
-					{
-						text: "lmao",
-						goTo: 3,
-						doneWell: "Noice(ayyyyy)",
-						canImprove: null
-					},
-					{
-						text: "rofl",
-						goTo: 4,
-						doneWell: null,
-						canImprove: "Get yo memes together yo(ayyyyy)"
-					}
-				]
-			},
-			{//3
-				type: "dialog",
-				speaker: "doctor",
-				goTo: 5,
-				text: "Bird bird"
-			},
-			{//4
-				type: "dialog",
-				speaker: "doctor",
-				goTo: 6,
-				text: "What's the good word?"
-			},
-			{//5
-				type: "choice",
-				choices: [
-					{
-						text: "Is a nerd?",
-						goTo: 7,
-						doneWell: null,
-						canImprove: "a nerd? wat?(bird bird)"
-					},
-					{
-						
-						text: "Is the word?",
-						goTo: 7,
-						doneWell: "Yes it is, well done(bird bird)",
-						canImprove: null
-					}
-				]
-			},
-			{//6
-				type: "choice",
-				choices: [
-					{
-						text: "THWG",
-						goTo: 7,
-						doneWell: "Piss on em!!!",
-						canImprove: null
-					},
-					{
-						text: "TMNT",
-						goTo: 7,
-						doneWell: null,
-						canImprove: "SHAME ON YOU, GET SOME SCHOOL SPIRIT PLZ"
-					}
-				]
-			},
-			{//7
-				type: "dialog",
-				speaker: "narrator",
-				goTo: null,
-				text: "Thank you for completing the Test Scenario!"
-			}
-		]
+		selectedStep: 0,
+		  title: 'Test Scenario',
+		  description: 'This Scenario is only for testing',
+		  steps: [
+		    {// 0
+		      type: 'dialog',
+		      speaker: 'narrator',
+		      goTo: 1,
+		      text: 'Welcome to the Test Scenario! Please press next.'
+		    },
+		    {// 1
+		      type: 'dialog',
+		      speaker: 'doctor',
+		      goTo: 2,
+		      text: 'Question'
+		    },
+		    {// 2
+		      type: 'choice',
+		      choices: [
+		        {
+		          text: 'yes',
+		          goTo: 3,
+		          doneWell: 'Very Good',
+		          canImprove: null
+		        },
+		        {
+		          text: 'no',
+		          goTo: 4,
+		          doneWell: null,
+		          canImprove: 'Not correct'
+		        }
+		      ]
+		    },
+		    {// 3
+		      type: 'dialog',
+		      speaker: 'doctor',
+		      goTo: 5,
+		      text: 'How are you?'
+		    },
+		    {// 4
+		      type: 'dialog',
+		      speaker: 'doctor',
+		      goTo: 6,
+		      text: 'Where are you from?'
+		    },
+		    {// 5
+		      type: 'choice',
+		      choices: [
+		        {
+		          text: 'Great!',
+		          goTo: 7,
+		          doneWell: null,
+		          canImprove: 'No further explaination?'
+		        },
+		        {
+		          text: 'Not doing to well.',
+		          goTo: 7,
+		          doneWell: 'Honest',
+		          canImprove: null
+		        }
+		      ]
+		    },
+		    {// 6
+		      type: 'choice',
+		      choices: [
+		        {
+		          text: 'Atlanta',
+		          goTo: 7,
+		          doneWell: 'Love me some ATL',
+		          canImprove: null
+		        },
+		        {
+		          text: 'Nashvegas',
+		          goTo: 7,
+		          doneWell: null,
+		          canImprove: 'But you are in ATL now?'
+		        }
+		      ]
+		    },
+		    {// 7
+		      type: 'dialog',
+		      speaker: 'narrator',
+		      goTo: null,
+		      text: 'Thank you for completing the Test Scenario!'
+		    }
+		  ]
 	})
 }
