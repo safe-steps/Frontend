744f36ab3d751747b41b54a76329a90bfabd4f46
Yea Won Yoon <ywjulie1013@gmail.com>
2017-03-11 18:23:07 -0500

    SafetyPlan submit button added

--- src/components/safetyPlan/SafetyPlan.jsx
+++ src/components/safetyPlan/SafetyPlan.jsx
@@ -1,11 +1,12 @@
 import React, {Component, PropTypes} from 'react';
-// import s from 'components/styles/index.scss';
+import s from 'components/styles/index.scss';
 import {connect} from 'react-redux';
-import {updatePlan} from 'redux/modules/safetyPlan.js';
+import {updatePlan, submitPlan} from 'redux/modules/safetyPlan.js';
+import {Link} from 'react-router';
 
 @connect(state => ({
   ...state.safetyPlan
-}), {updatePlan})
+}), {updatePlan, submitPlan})
 export default class SafetyPlan extends Component {
   static propTypes = {
     escapeRoutes: PropTypes.string,
@@ -13,7 +14,8 @@ export default class SafetyPlan extends Component {
     locationsAndContacts: PropTypes.string,
     argumentLocations: PropTypes.string,
     codeword: PropTypes.string,
-    updatePlan: PropTypes.func
+    updatePlan: PropTypes.func,
+    submitPlan: PropTypes.func
   }
   planChanged = (e) => {
     if (e.target.name === 'escapeRoutes_input') {
@@ -52,6 +54,16 @@ export default class SafetyPlan extends Component {
         <div><label htmlFor="locationsAndContacts_input">Locations and contacts to go to in case of emergency (e.g. family, friend)</label> <input type="text" id="locationsAndContacts_input" name="locationsAndContacts_input" value={this.props.locationsAndContacts} onChange={(e) => this.planChanged(e)}/></div>
         <div><label htmlFor="argumentLocations_input">Locations that are safer places to have arguments (avoid kitchens or bathrooms)</label> <input type="text" id="argumentLocations_input" name="argumentLocations_input" value={this.props.argumentLocations} onChange={(e) => this.planChanged(e)}/></div>
         <div><label htmlFor="codeword_input">A codeword for their children, friends, and trusted ones to know when to call for help</label> <input type="text" id="codeword_input" name="codeword_input" value={this.props.codeword} onChange={(e) => this.planChanged(e)}/></div>
+      <Link to={'/'} className={s.button} onClick={() => {
+        this.props.submitPlan({
+          escapeRoutes: this.props.escapeRoutes,
+          itemsToCollect: this.props.itemsToCollect,
+          locationsAndContacts: this.props.locationsAndContacts,
+          argumentLocations: this.props.argumentLocations,
+          codeword: this.props.codeword
+        });
+      }}>Submit Safety Plan</Link>
+      <Link to={'/'} className={s.button}>Back to Home</Link>
       </div>
     );
   }
--- src/redux/modules/safetyPlan.js
+++ src/redux/modules/safetyPlan.js
@@ -1,5 +1,7 @@
 const UPDATE_PLAN = 'safetyPlan/UPDATE_PLAN';
-
+const SUBMIT_PLAN = 'safetyPlan/SUBMIT_PLAN';
+const SUBMIT_PLAN_SUCCESS = 'scenarioEditor/SUBMIT_PLAN_SUCCESS';
+const SUBMIT_PLAN_FAIL = 'scenarioEditor/SUBMIT_PLAN_FAIL';
 
 // import clone from 'lodash';
 
@@ -8,13 +10,29 @@ const initialState = {
   itemsToCollect: '',
   locationsAndContacts: '',
   argumentLocations: '',
-  codeword: ''
+  codeword: '',
+  submitting: false
 };
 
 export default function reducer(state = initialState, action = {}) {
   switch (action.type) {
     case UPDATE_PLAN:
       return action.obj;
+    case SUBMIT_PLAN:
+      return {
+        ...state,
+        submitting: true
+      };
+    case SUBMIT_PLAN_SUCCESS:
+      return {
+        ...state,
+        submitting: false
+      };
+    case SUBMIT_PLAN_FAIL:
+      return {
+        ...state,
+        submitting: false
+      };
     default:
       return state;
   }
@@ -24,5 +42,15 @@ export function updatePlan(obj) {
   return {
     type: UPDATE_PLAN,
     obj
+  };
+}
+
+export function submitPlan(json) {
+  console.log(json);
+  return {
+    types: [SUBMIT_PLAN, SUBMIT_PLAN_SUCCESS, SUBMIT_PLAN_FAIL],
+    promise: (client) => client.post('/safetyPlan', {
+      data: json
+    })
   };
 }
