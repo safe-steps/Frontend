376dbe82636a99de9f699ae0244356982e99cfdb
daltenbern3 <darrington.altenbern@gmail.com>
2017-03-31 11:07:21 -0400

    Added skeleton for training page

---
+++ src/components/Training/Training.jsx
@@ -0,0 +1,11 @@
+import React, {Component, PropTypes} from 'react';
+import s from 'components/styles/index.scss';
+import {Link} from 'react-router';
+
+export default class SafetyPlan extends Component {
+  render() {
+    return (
+      
+    );
+  }
+}
